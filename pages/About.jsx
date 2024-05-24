ca53a0da5c587db41906f442d6a81f60a50126b1
Naushikr <naushikr7@gmail.com>
2024-05-23 22:35:32 -0400

    addin files and gitignore

---
+++ pages/About.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+export default function About(){
+    return(
+        <h2>Don’t squeeze in a sedan when you could relax in a van.</h2>
+    )
+}
---
+++ pages/Home.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+export default function Home(){
+    return(
+        <h2>You got the travel plans, we got the travel vans.</h2>
+    )
+}
